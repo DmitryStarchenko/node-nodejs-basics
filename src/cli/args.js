689f3697c080f53fc7691dbb2a5d6a01ce7a8819
Dmitry <Dololob92@gmail.com>
2025-10-19 18:21:41 +0300

    feat: add args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-  // Write your code here
+  const ARGS = process.argv.slice(2);
+  let result = [];
+  for (let i = 0; i < ARGS.length; i += 2) {
+    const prop = ARGS[i];
+    const value = ARGS[i + 1];
+
+    if (prop && prop.startsWith("--") && value !== undefined) {
+      const propName = prop.slice(2);
+      result.push(`${propName} is ${value}`);
+    }
+  }
+  if (result.length > 0) {
+    console.log(result.join(", "));
+  }
 };
 
 parseArgs();
