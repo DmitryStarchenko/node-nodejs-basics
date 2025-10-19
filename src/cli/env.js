c076e7c9305593f0905875d426dbceb89330ac2c
Dmitry <Dololob92@gmail.com>
2025-10-19 18:08:31 +0300

    feat: add env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-  // Write your code here
+  const ENV = process.env;
+  const rssVars = Object.entries(ENV).filter(([key]) => key.startsWith("RSS_"));
+
+  const result = rssVars.map(([key, value]) => `${key}=${value}`).join("; ");
+  console.log(result);
 };
 
 parseEnv();
