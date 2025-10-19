929ff1777be8ab48cd629b5ab67b5c429533d21c
Dmitry <Dololob92@gmail.com>
2025-10-19 17:36:24 +0300

    feat: add rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+
+const CURRENT_PATH = "src/fs/files/wrongFilename.txt";
+const NEW_PATH = "src/fs/files/properFilename.md";
+
 const rename = async () => {
-  // Write your code here
+  fs.access(NEW_PATH, fs.constants.F_OK, (err) => {
+    if (err) {
+      fs.rename(CURRENT_PATH, NEW_PATH, (err) => {
+        if (err) throw new Error("FS operation failed");
+      });
+    } else {
+      throw new Error("FS operation failed");
+    }
+  });
 };
 
 await rename();
