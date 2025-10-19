866aec54f77414aa0f031b6c37d9b9cd38f89501
Dmitry <Dololob92@gmail.com>
2025-10-19 16:34:07 +0300

    feat: add create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+
+const PATH = "src/fs/files/fresh.txt";
+const DATA = "I am fresh and young";
+
 const create = async () => {
-  // Write your code here
+  fs.readFile(PATH, (error) => {
+    if (error) {
+      fs.writeFile(PATH, DATA, () => {});
+      console.log("create file");
+    } else {
+      throw new Error("FS operation failed");
+    }
+  });
 };
 
 await create();
