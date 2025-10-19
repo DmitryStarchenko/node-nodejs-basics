b4e5e39dfbcf7432aaebad5243dd4b2f848418b1
Dmitry <Dololob92@gmail.com>
2025-10-19 17:28:08 +0300

    feat: add copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+
+const CURRENT_PATH = "src/fs/files";
+const COPY_PATH = "src/fs/files-copy";
+
 const copy = async () => {
-  // Write your code here
+  fs.access(COPY_PATH, fs.constants.F_OK, (err) => {
+    if (err) {
+      fs.cp(CURRENT_PATH, COPY_PATH, { recursive: true }, (err) => {
+        if (err) throw new Error("FS operation failed");
+      });
+    } else {
+      throw new Error("FS operation failed");
+    }
+  });
 };
 
 await copy();
