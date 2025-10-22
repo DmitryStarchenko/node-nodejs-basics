70a268bc237baee7a1e03a5f9e07cd9162307d9c
Dmitry <Dololob92@gmail.com>
2025-10-22 23:54:30 +0300

    feat: add stream write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,25 @@
+import fs from "fs";
+
+const FILE_PATH = "src/streams/files/fileToWrite.txt";
+
 const write = async () => {
-  // Write your code here
+  console.log("To save, enter 'save'");
+  console.log("Enter data:");
+
+  const stream = fs.createWriteStream(FILE_PATH, { flags: "a" });
+  let fullData = "";
+
+  process.stdin.setEncoding("utf8");
+  process.stdin.on("data", (data) => {
+    const save = data.trim();
+    if (save === "save") process.exit(0);
+    fullData += data;
+    stream.write(fullData);
+  });
+
+  process.stdin.on("end", () => {
+    stream.end();
+  });
 };
 
 await write();
