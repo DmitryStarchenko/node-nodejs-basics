33eebfb448c01b7246e7ec30c28f98d42abfb786
Dmitry <Dololob92@gmail.com>
2025-10-22 22:15:36 +0300

    feat: add calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import crypto from "crypto";
+
 const calculateHash = async () => {
-  // Write your code here
+  const filename = "src/hash/files/fileToCalculateHashFor.txt";
+  const fileStream = fs.createReadStream(filename);
+  const hash = crypto.createHash("sha256");
+
+  fileStream.on("data", (data) => {
+    hash.update(data);
+  });
+
+  fileStream.on("end", () => {
+    const fileHash = hash.digest("hex");
+    console.log(`File hash: ${fileHash}`);
+  });
 };
 
 await calculateHash();
