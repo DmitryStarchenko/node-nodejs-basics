d0db2cf2b7d5200d3539e7e98f7a696a852960bc
Dmitry <Dololob92@gmail.com>
2025-10-23 22:18:57 +0300

    feat: add zip compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,15 @@
+import { createGzip } from "zlib";
+import fs from "fs";
+
+const FILE_PATH = "src/zip/files/fileToCompress.txt";
+const ZIP_FILE_PATH = "src/zip/files/archive.gz";
+
 const compress = async () => {
-  // Write your code here
+  const gzip = createGzip();
+  const source = fs.createReadStream(FILE_PATH);
+  const destination = fs.createWriteStream(ZIP_FILE_PATH);
+
+  source.pipe(gzip).pipe(destination);
 };
 
 await compress();
