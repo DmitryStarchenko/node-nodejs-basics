90747cc323eff5780eebd777daaa1e203004e122
Dmitry <Dololob92@gmail.com>
2025-10-23 22:39:03 +0300

    feat: add zip decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import { createGunzip } from "zlib";
+import fs from "fs";
+
+const FILE_PATH = "src/zip/files/fileToCompress.txt";
+const ZIP_FILE_PATH = "src/zip/files/archive.gz";
+
 const decompress = async () => {
-  // Write your code here
+  const gzip = createGunzip();
+  const source = fs.createReadStream(ZIP_FILE_PATH);
+  const destination = fs.createWriteStream(FILE_PATH);
+
+  source.pipe(gzip).pipe(destination);
 };
 
 await decompress();
