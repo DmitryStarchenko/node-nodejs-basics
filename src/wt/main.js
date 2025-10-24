b71c5e8380d1705992890795dd03f4f5dac19ebb
Dmitry <Dololob92@gmail.com>
2025-10-24 22:04:05 +0300

    feat: add wt main

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const WORKER_PATH = join(dirname(fileURLToPath(import.meta.url)), "worker.js");
+
 const performCalculations = async () => {
-  // Write your code here
+  const cpu = cpus().length;
+  const workers = [];
+
+  const workerPromises = Array.from({ length: cpu }, (_, i) => {
+    return new Promise((resolve) => {
+      const worker = new Worker(WORKER_PATH);
+      workers.push(worker);
+      const number = 10 + i;
+      worker.postMessage(number);
+
+      worker.on("message", (result) => {
+        resolve(result);
+      });
+
+      worker.on("error", () => {
+        resolve({ status: "error", data: null });
+      });
+    });
+  });
+
+  const workerResults = await Promise.all(workerPromises);
+
+  console.log(workerResults);
+
+  workers.forEach((worker) => {
+    worker.terminate();
+  });
 };
 
 await performCalculations();
